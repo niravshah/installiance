91a873be05c99e0128d2e523a576526b49d2e3c6
shahn2 <nirav.shah@capita.co.uk>
2016-12-08 15:12:38 +0000

    M

--- routes/instagram.js
+++ routes/instagram.js
@@ -17,7 +17,7 @@ module.exports = function (app) {
                 res.send("Didn't work");
             } else {
                 console.log('Yay! Access token is ' + result.access_token);
-                ig.user_self([{access_token:result.access_token}],function (err, medias, pagination, remaining, limit) {
+                ig.user_self({access_token:result.access_token},function (err, medias, pagination, remaining, limit) {
                     console.log('Error', err);
                     console.log('Medias', medias);
                     console.log(pagination, remaining, limit);
