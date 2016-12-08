e92d7fdae841119dad64ff92a0867c0470c59744
shahn2 <nirav.shah@capita.co.uk>
2016-12-08 14:19:04 +0000

    M

--- routes/instagram.js
+++ routes/instagram.js
@@ -4,13 +4,13 @@ module.exports = function (app) {
 
     ig.use({ client_id: 'e942353eeb2f4c4eb38d5ce059ee5b35', client_secret: '54fc1a4b2b8e45a6a04016657b03bfa6' });
 
-    var redirect_uri = 'http://yoursite.com/handle_auth';
+    var redirect_uri = 'https://allyx.herokuapp.com/welcome';
 
     app.get('/authorize_user', function (req, res) {
         res.redirect(ig.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
     });
 
-    app.get('/handle_auth', function (req, res) {
+    app.get('/welcome', function (req, res) {
         ig.authorize_user(req.query.code, redirect_uri, function (err, result) {
             if (err) {
                 console.log(err.body);
