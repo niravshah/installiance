67ceb1d4e6c4fbe94707096d92028cf0e60e3bc4
shahn2 <nirav.shah@capita.co.uk>
2016-12-08 14:11:46 +0000

    M

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ var env = process.env.NODE_ENV || 'dev';
 var config = require('./config')[env];
 console.log("ENV:", env);
 
-var mongoose = require('mongoose');
-mongoose.plugin(require('./models/plugins/tenant'));
-mongoose.connect(config.mongoUrl);
+//var mongoose = require('mongoose');
+//mongoose.connect(config.mongoUrl);
 
-var mongo_express = require('mongo-express/lib/middleware');
-app.use('/mongo_express', mongo_express(config.mongo_express_config));
+//var mongo_express = require('mongo-express/lib/middleware');
+//app.use('/mongo_express', mongo_express(config.mongo_express_config));
 
 var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,14 +20,14 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var swig = require('swig');
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('view cache', false);
-swig.setDefaults({cache: false});
+swig.setDefaults({ cache: false });
 
 var logger = require('morgan');
 app.use(logger('dev'));
@@ -36,13 +35,10 @@ app.use(logger('dev'));
 var passport = require('passport');
 app.use(passport.initialize());
 
-var initPassport = require('./routes/passport/init');
-initPassport(passport);
-
+require('./routes/passport/init')(passport);
+require('./routes/instagram')(app);
 
 require('./routes/login')(app);
-require('./routes/index')(app,passport);
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -55,21 +51,23 @@ if (env === 'dev') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.json({
-                     message: "Error Response",
-                     errorMessage: err.message,
-                     error: err
-                 });
+            message: "Error Response",
+            errorMessage: err.message,
+            error: err
+        });
     });
 } else {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.json({
-                     message: "Error Response",
-                     errorMessage: err.message
-                 });
+            message: "Error Response",
+            errorMessage: err.message
+        });
     });
 }
 
-app.listen(14000, function () {
-    console.log('AllyX App listening on port 12000!');
+app.set('port', (process.env.PORT || 12000));
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
