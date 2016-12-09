dc5bc7de84109138fce4a9380d87aa96c3eb9022
shahnirav <shah.nirav@me.com>
2016-12-09 08:14:33 +0000

    M

--- models/user.js
+++ models/user.js
@@ -7,15 +7,7 @@ var userSchema = new Schema({
     email: {type: String,required: true},
     password: {type: String,required: true},
     avatar: {type: String,required: true},
-    title: {type: String},
-    userRoles: {type: Array, default: ['USER']},
-    memberships: {type: Array, default: []},
-    projectRoles:{type:Schema.Types.Mixed},
-    badges:{type:Array, default:[]},
-    profileSet: {type:Boolean, default:false},
-    profile:{type: mongoose.Schema.ObjectId, ref: 'Profile'},
     resetPassword: {type:Boolean, default:true},
-    aboutMe:{type:String}
 });
 
 module.exports = mongoose.model('User', userSchema);    
--- routes/instagram.js
+++ routes/instagram.js
@@ -1,40 +1,82 @@
 var ig = require('instagramapi').instagram();
-
+var async = require('asyncjs');
 module.exports = function (app) {
 
-    ig.use({ client_id: 'e942353eeb2f4c4eb38d5ce059ee5b35', client_secret: '54fc1a4b2b8e45a6a04016657b03bfa6' });
+    ig.use({
+               client_id: 'e942353eeb2f4c4eb38d5ce059ee5b35',
+               client_secret: '54fc1a4b2b8e45a6a04016657b03bfa6'
+           });
 
     var redirect_uri = 'https://allyx.herokuapp.com/welcome';
 
     app.get('/authorize_user', function (req, res) {
-        res.redirect(ig.get_authorization_url(redirect_uri, { scope: ['basic', 'follower_list'], state: 'a state' }));
+        res.redirect(ig.get_authorization_url(redirect_uri, {
+            scope: ['basic', 'follower_list'],
+            state: 'a state'
+        }));
     });
 
     app.get('/welcome', function (req, res) {
         ig.authorize_user(req.query.code, redirect_uri, function (err, result) {
             if (err) {
                 console.log(err.body);
-                res.send("Didn't work");
+                res.render('error', {
+                    message: "We could not authorize your request with Instagram!",
+                    details: err.body
+                })
             } else {
                 console.log('Yay! Access token is ' + result.access_token);
-                ig.user_self({ access_token: result.access_token }, function (err, medias, pagination, remaining, limit) {
-                    console.log('Error', err);
-                    console.log('Self', medias);
-                    console.log(pagination, remaining, limit);
 
-                    ig.user_followers({ access_token: result.access_token }, function (err, medias, pagination, remaining, limit) {
-                        console.log('Error', err);
-                        console.log('Followers', medias);
-                        console.log(pagination, remaining, limit);
+                async.parallel([
+                                   function (cb) {
+                                       ig.user_self({access_token: result.access_token},
+                                                    function (err, userInfo, pagination, remaining,
+                                                              limit) {
+                                                        console.log('Error', err);
+                                                        console.log('Self', userInfo);
+                                                        console.log(pagination, remaining, limit);
+                                                        cb(err, userInfo);
+                                                    });
+                                   },
+                                   function (cb) {
+                                       ig.user_self_media_recent(
+                                           {access_token: result.access_token},
+                                           function (err, userInfo, pagination, remaining,
+                                                     limit) {
+                                               console.log('Error', err);
+                                               console.log('Self', userInfo);
+                                               console.log(pagination, remaining, limit);
+                                               cb(err, userInfo);
+                                           });
+                                   }],
+                               function (err, results) {
 
-                        ig.user_self_media_recent({}, { access_token: result.access_token }, function (err, medias, pagination, remaining, limit) {
-                            console.log('Error', err);
-                            console.log('Medias', medias);
-                            console.log(pagination, remaining, limit);
-                            res.send(medias);
-                        });
-                    });
-                });
+                                   if (err) {
+                                       res.render('error',
+                                                  {
+                                                      message: 'There was an error retreiving your details from Instagram',
+                                                      details: err
+                                                  })
+
+                                   }
+                                   if (results.length < 2) {
+                                       res.render('error',
+                                                  {
+                                                      message: 'We could not retrieve all your details from Instagram',
+                                                      details: ''
+                                                  })
+                                   } else {
+
+                                       var follows = result[0].data.counts.follows;
+                                       var followed_by = result[0].data.counts.followed_by;
+                                       if (followed_by > follows) {
+                                           res.render('successful');
+                                       } else {
+                                           res.render('unsuccesful');
+                                       }
+
+                                   }
+                               });
 
             }
         });
