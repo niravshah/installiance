dd0ecf1fbce183f88e218bc4bc63c9b8d41070f0
shahn2 <nirav.shah@capita.co.uk>
2016-12-09 11:01:33 +0000

    M

--- routes/instagram.js
+++ routes/instagram.js
@@ -3,9 +3,9 @@ var async = require('async');
 module.exports = function (app) {
 
     ig.use({
-               client_id: 'e942353eeb2f4c4eb38d5ce059ee5b35',
-               client_secret: '54fc1a4b2b8e45a6a04016657b03bfa6'
-           });
+        client_id: 'e942353eeb2f4c4eb38d5ce059ee5b35',
+        client_secret: '54fc1a4b2b8e45a6a04016657b03bfa6'
+    });
 
     var redirect_uri = 'https://allyx.herokuapp.com/welcome';
 
@@ -28,58 +28,63 @@ module.exports = function (app) {
                 console.log('Yay! Access token is ' + result.access_token);
 
                 async.parallel([
-                                   function (cb) {
-                                       ig.user_self({access_token: result.access_token},
-                                                    function (err, userInfo, pagination, remaining,
-                                                              limit) {
-                                                        console.log('Error', err);
-                                                        console.log('Self', userInfo);
-                                                        console.log(pagination, remaining, limit);
-                                                        cb(err, userInfo);
-                                                    });
-                                   },
-                                   function (cb) {
-                                       ig.user_self_media_recent({},{access_token: result.access_token},
-                                           function (err, userInfo, pagination, remaining,
-                                                     limit) {
-                                               console.log('Error', err);
-                                               console.log('Self', userInfo);
-                                               console.log(pagination, remaining, limit);
-                                               cb(err, userInfo);
-                                           });
-                                   }],
-                               function (err, results) {
-                                   console.log(err, results);
+                        function (cb) {
+                            ig.user_self({ access_token: result.access_token },
+                                function (err, userInfo, pagination, remaining,
+                                          limit) {
+                                    console.log('Error', err);
+                                    console.log('Self', userInfo);
+                                    console.log(pagination, remaining, limit);
+                                    cb(err, userInfo);
+                                });
+                        },
+                        function (cb) {
+                            ig.user_self_media_recent({}, { access_token: result.access_token },
+                                function (err, userInfo, pagination, remaining,
+                                          limit) {
+                                    console.log('Error', err);
+                                    console.log('Self', userInfo);
+                                    console.log(pagination, remaining, limit);
+                                    cb(err, userInfo);
+                                });
+                        }],
+                    function (err, results) {
+                        console.log(err, results);
 
-                                   if (err) {
-                                       res.render('error',
-                                                  {
-                                                      message: 'There was an error retrieving your details from Instagram',
-                                                      details: err
-                                                  })
+                        if (err) {
+                            console.log("Error Loop");
+                            res.render('error',
+                                {
+                                    message: 'There was an error retrieving your details from Instagram',
+                                    details: err
+                                })
 
-                                   }
-                                   if (results.length < 2) {
-                                       res.render('error',
-                                                  {
-                                                      message: 'We could not retrieve all your details from Instagram',
-                                                      details: ''
-                                                  })
-                                   } else {
+                        }
 
-                                       var follows = result[0].counts.follows;
-                                       var followed_by = result[0].counts.followed_by;
-                                       console.log(follows, followed_by);
-                                       if (followed_by > follows) {
-                                           console.log("Returning Successful");
-                                           res.render('successful');
-                                       } else {
-                                           console.log("Returning Unsuccessful");
-                                           res.render('unsuccessful');
-                                       }
+                        console.log(results.length);
 
-                                   }
-                               });
+                        if (results.length < 2) {
+
+                            res.render('error',
+                                {
+                                    message: 'We could not retrieve all your details from Instagram',
+                                    details: ''
+                                })
+                        } else {
+
+                            var follows = result[0].counts.follows;
+                            var followed_by = result[0].counts.followed_by;
+                            console.log(follows, followed_by);
+                            if (followed_by > follows) {
+                                console.log("Returning Successful");
+                                res.render('successful');
+                            } else {
+                                console.log("Returning Unsuccessful");
+                                res.render('unsuccessful');
+                            }
+
+                        }
+                    });
 
             }
         });
