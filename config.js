3d45b8b2939bf09d049d6904a4b5fca120c280c4
shahn2 <nirav.shah@capita.co.uk>
2016-12-14 12:39:01 +0000

    M

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ var config = [];
 
 config['dev'] = {
     mongoUrl: 'mongodb://localhost/engageapp',
-    followed_by_count: 100
+    followed_by_count: 50
 };
 
 config['production'] = {
     mongoUrl: 'mongodb://installiance:installiance6383@ds119568.mlab.com:19568/heroku_9s89sx1b',
-    followed_by_count: 5000
+    followed_by_count: 50
 };
 
 module.exports = config;
