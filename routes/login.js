43624bbffb1cf0300f86ab0f5de1712a56863ba7
shahn2 <nirav.shah@capita.co.uk>
2016-12-14 15:40:53 +0000

    M

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,5 @@
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        res.render('login');
+        res.render('home');
     });
-
-    app.get('/login', function (req, res) {
-        res.render('login');
-    });
-
 };
