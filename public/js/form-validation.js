6066c578d53c159a983b79a218fccd20fad39abd
shahn2 <nirav.shah@capita.co.uk>
2016-12-15 13:07:48 +0000

    commit

--- public/js/form-validation.js
+++ public/js/form-validation.js
@@ -3,14 +3,13 @@ $(function () {
     $("form[name='registration']").validate({
         // Specify validation rules
         rules: {
-
+            password:{
+                required: true
+            },
             email: {
                 required: true,
-                // Specify that email should be validated
-                // by the built-in "email" rule
                 email: true
             }
-
         },
         // Specify validation error messages
         messages: {
@@ -19,7 +18,38 @@ $(function () {
         // Make sure the form is submitted to the destination defined
         // in the "action" attribute of the form when valid
         submitHandler: function (form) {
-            form.submit();
+            var shortid = $('#onboard-btn').data('shortid');
+            var url = '/api/user/'+shortid + '/email';
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(form).serialize(),
+                success: function () {
+                    $(form).html("<div id='message'></div>");
+                    $('#message').html("<h2>Your request is on the way!</h2>")
+                        .append("<p>someone</p>")
+                        .hide()
+                        .fadeIn(1500, function () {
+                            $('#message').append("<img id='checkmark' src='/    images/ok.png' />");
+                        });
+                },
+                error:function(xhr, error,thrownError){
+                    $(form).html("<div id='message'></div>");
+                    $('#message').html("<h2 class='error'>" + thrownError + "</h2>")
+                        .append("<p>" + xhr.responseJSON.errorMessage + "</p>")
+                        .hide()
+                        .fadeIn(1500, function () {
+                            $('#message').append("<img id='checkmark' src='/images/cross.png' />");
+                        });
+                }
+            });
+            return false;
+        },
+        invalidHandler: function (event, validator) {
+            var errors = validator.numberOfInvalids();
+            if (errors) {
+                console.log('Errors:',errors);
+            }
         }
     });
 });
