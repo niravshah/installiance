f65a6a87fc1c88e88ecf2a0e975879ec8c68632c
shahn2 <nirav.shah@capita.co.uk>
2016-12-19 16:15:37 +0000

    commit

--- public/js/angular/controllers/allianceController.js
+++ public/js/angular/controllers/allianceController.js
@@ -1,5 +1,28 @@
 app.controller('allianceController', function ($http, $scope) {
     $scope.init = function () {
+
+
+
     };
     $scope.init();
+
+    $scope.myModel = 1;
+
+    $scope.myOptions = [
+        {id: 1, title: 'Spectrometer'},
+        {id: 2, title: 'Star Chart'},
+        {id: 3, title: 'Laser Pointer'}
+    ];
+
+    $scope.myConfig = {
+        create: true,
+        valueField: 'id',
+        labelField: 'title',
+        delimiter: '|',
+        placeholder: 'Pick something',
+        onInitialize: function(selectize){
+            // receives the selectize object as an argument
+        },
+        // maxItems: 1
+    };
 });
--- public/js/angular/homeApp.js
+++ public/js/angular/homeApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('influenceAllyHome', ['ui.router']);
+var app = angular.module('influenceAllyHome', ['ui.router','selectize']);
 
 app.config(function ($interpolateProvider, $stateProvider, $urlRouterProvider) {
 
