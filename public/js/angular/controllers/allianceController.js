0f7a4d7d03fa14ceae96368d1f9446ccc0296c8e
shahnirav <shah.nirav@me.com>
2016-12-19 08:54:10 +0000

    m

---
+++ public/js/angular/controllers/allianceController.js
@@ -0,0 +1,5 @@
+app.controller('allianceController', function ($http, $scope) {
+    $scope.init = function () {
+    };
+    $scope.init();
+});
--- public/js/angular/homeApp.js
+++ public/js/angular/homeApp.js
@@ -1,5 +1,18 @@
 var app = angular.module('influenceAllyHome', ['ui.router']);
 
-app.config(function ($interpolateProvider) {
+app.config(function ($interpolateProvider, $stateProvider, $urlRouterProvider) {
+
     $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/js/angular/partials/home.html'
+        })
+        .state('alliance',{
+            url:'/alliance/new',
+            templateUrl: '/js/angular/partials/alliance/new.html',
+            controller: 'allianceController'
+        })
 });
