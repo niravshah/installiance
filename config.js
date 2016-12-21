a48ed580dc83f7bfe38ac6ea2d7c32592600b114
shahn2 <nirav.shah@capita.co.uk>
2016-12-21 11:01:32 +0000

    commit

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ config['dev'] = {
 config['production'] = {
     mongoUrl: 'mongodb://installiance:installiance6383@ds119568.mlab.com:19568/heroku_9s89sx1b',
     followed_by_count: 50,
-    emails:false
+    emails:true
 };
 
 module.exports = config;
